534c15fac5328f7e103cf775691d6beabef6c31e
GiorgiKemo <119447872+GiorgiKemo@users.noreply.github.com>
2025-04-07 02:06:17 +0400

    Fix CORS configuration to avoid path-to-regexp error

--- server/index.js
+++ server/index.js
@@ -18,10 +18,27 @@ const View = require('./models/viewModel');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['https://airdrops-geo.onrender.com', 'http://localhost:5173'],
-  credentials: true
-}));
+app.use(cors());
+
+// Configure CORS for specific origins
+app.use((req, res, next) => {
+  const allowedOrigins = ['https://airdrops-geo.onrender.com', 'http://localhost:5173'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
